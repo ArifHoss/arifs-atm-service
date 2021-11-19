a08068ccb8040962dadd70ef50a386af19664c75
Arif <hossaininfo@ymail.com>
2021-11-19 14:35:15 +0100

    Created menu for ATM service

---
+++ src/main/java/ATMMenu.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ATMMenu {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String [] args){
+        while(true){
+            System.out.println("=================================");
+            System.out.println("\n1. Withdraw");
+            System.out.println("2. Unset");
+            System.out.println("3. Unset");
+            System.out.println("4. Unset");
+            System.out.println("0. Exit");
+            System.out.println("=================================");
+            System.out.print("Please make a choice: ");
+
+            int choice = sc.nextInt();
+            if(choice == 1){
+                System.out.println("=================================");
+                System.out.print("How much do you need to withdraw: ");
+                int chooseWithdrawAmount = sc.nextInt();
+//                withdraw(chooseWithdrawAmount);
+            }else if(choice == 2){
+                System.out.println("=================================");
+                System.out.println("This function is currently unavailable");
+            }else if(choice == 3){
+                System.out.println("=================================");
+                System.out.println("This function is currently unavailable");
+            }else if(choice == 4){
+                System.out.println("=================================");
+                System.out.println("This function is currently unavailable");
+            }else if(choice == 0){
+                System.out.println("=================================");
+                System.out.println("Good Bye! See you soon.");
+                System.exit(0);
+            }else{
+                System.out.println("Wrong choice! Choose again.");
+            }
+
+        }
+    }
+}
