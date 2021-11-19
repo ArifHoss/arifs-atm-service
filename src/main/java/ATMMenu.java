a85b79ff8ad884fc2bdb0cf738294c3c7e0465a9
Arif <hossaininfo@ymail.com>
2021-11-19 15:21:01 +0100

    created checkBanknotes and calculateBalance method

--- src/main/java/ATMMenu.java
+++ src/main/java/ATMMenu.java
@@ -9,8 +9,8 @@ public class ATMMenu {
             System.out.println("=================================");
             System.out.println("\n1. Withdraw");
             System.out.println("2. Unset");
-            System.out.println("3. Unset");
-            System.out.println("4. Unset");
+            System.out.println("3. Check banknotes");
+            System.out.println("4. Check Balance");
             System.out.println("0. Exit");
             System.out.println("=================================");
             System.out.print("Please make a choice: ");
@@ -26,10 +26,11 @@ public class ATMMenu {
                 System.out.println("This function is currently unavailable");
             }else if(choice == 3){
                 System.out.println("=================================");
-                System.out.println("This function is currently unavailable");
+                System.out.println("Banknotes left.");
+                AtmServiceFunction.checkBanknotes();
             }else if(choice == 4){
                 System.out.println("=================================");
-                System.out.println("This function is currently unavailable");
+                AtmServiceFunction.calculateBalance();
             }else if(choice == 0){
                 System.out.println("=================================");
                 System.out.println("Good Bye! See you soon.");
--- src/main/java/AtmServiceFunction.java
+++ src/main/java/AtmServiceFunction.java
@@ -2,13 +2,28 @@ public class AtmServiceFunction {
 
     /*
     Banknotes available in ATM
-    Amount of each banknotes
+    Amount of each banknote
     The Counter
     */
 
     static final int [] banknotes = {1000, 500, 100};
     static int [] amountOfEachNotes = {2, 3, 5};
     static int [] counter = {0, 0, 0};
+    static int balance;
+
+    public static void checkBanknotes(){
+        for (int i = 0; i < banknotes.length; i++) {
+            System.out.println(banknotes[i]+" * "+amountOfEachNotes[i]+ " = "+banknotes[i]*amountOfEachNotes[i]);
+        }
+    }
+
+    public static void calculateBalance(){
+        for (int i = 0; i < banknotes.length; i++) {
+            balance = balance + (banknotes[i]*amountOfEachNotes[i]);
+//            System.out.println(banknotes[i]+" * "+amountOfEachNotes[i] +" = "+ banknotes[i]*amountOfEachNotes[i] );
+        }
+            System.out.println("Total balance = "+balance);
+    }
 
 
 }
