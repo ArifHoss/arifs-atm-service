a5c291bcd7ffd3798a7f38f5c99f80865468edb1
Arif <hossaininfo@ymail.com>
2021-11-19 14:58:17 +0100

    created banknotes, amountOfEachNotes and counter array

--- src/main/java/AtmServiceFunction.java
+++ src/main/java/AtmServiceFunction.java
@@ -1,2 +1,14 @@
 public class AtmServiceFunction {
+
+    /*
+    Banknotes available in ATM
+    Amount of each banknotes
+    The Counter
+    */
+
+    static final int [] banknotes = {1000, 500, 100};
+    static int [] amountOfEachNotes = {2, 3, 5};
+    static int [] counter = {0, 0, 0};
+
+
 }
