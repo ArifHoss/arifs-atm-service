bd7a6d9c67bc72a42aa2a6203edbd068257918e5
Arif <hossaininfo@ymail.com>
2021-11-24 17:23:04 +0100

    update menu and withdraw method

--- src/main/java/ATMMenu.java
+++ src/main/java/ATMMenu.java
@@ -5,45 +5,54 @@ public class ATMMenu {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String [] args){
+
         while(true){
-            System.out.println("=================================");
-            System.out.println("1. Withdraw");
-            System.out.println("2. Check total balance");
-            System.out.println("3. Check available banknotes");
-            System.out.println("4. Show left banknotes");
-            System.out.println("0. Exit");
-            System.out.println("=================================");
-            System.out.print("Please make a choice: ");
+            System.out.println("""
+                \n=================================
+                           ATM Services
+                =================================
+                1. Withdraw
+                2. Check total balance
+                3. Check available banknotes
+                4. Show left banknotes
+                0. Exit
+                =================================
+                """);
 
+            System.out.print("Please make a choice:");
             int choice = sc.nextInt();
-            if(choice == 1){
-                System.out.println("=================================");
-                System.out.print("How much do you need to withdraw: ");
-                int chooseWithdrawAmount = sc.nextInt();
-                AtmServiceFunction.withdraw(chooseWithdrawAmount);
 
-            }else if(choice == 2){
-                System.out.println("=================================");
-                AtmServiceFunction.showTotalBalance();
-
-            }else if(choice == 3){
-                System.out.println("=================================");
-                System.out.println("This are the bills available in this ATM.");
-                AtmServiceFunction.checkBanknotes();
-
-            }else if(choice == 4){
-                System.out.println("=================================");
-                AtmServiceFunction.showLeftBanknotes();
-
-            }else if(choice == 0){
-                System.out.println("=================================");
-                System.out.println("Good Bye! See you soon.");
-                System.exit(0);
-
-            }else{
-                System.out.println("Wrong choice! Choose again.");
+            switch (choice) {
+                case 1 -> initiateWithdraw();
+                case 2 -> {
+                    System.out.println("=================================");
+                    AtmServiceFunction.showTotalBalance();
+                }
+                case 3 -> {
+                    System.out.println("=================================");
+                    System.out.println("This are the bills available in this ATM.");
+                    AtmServiceFunction.checkBanknotes();
+                }
+                case 4 -> {
+                    System.out.println("=================================");
+                    AtmServiceFunction.showLeftBanknotes();
+                }
+                case 0 -> {
+                    System.out.println("=================================");
+                    System.out.println("Good Bye! See you soon.");
+                    System.exit(0);
+                }
+                default -> System.out.println("Wrong choice! Choose again.");
             }
 
         }
+    }
+
+    private static void initiateWithdraw() {
+        System.out.println("=================================");
+        System.out.print("How much do you need to withdraw: ");
+        int chooseWithdrawAmount = sc.nextInt();
+        AtmServiceFunction.numberOfEachBillToPayOut = new int[]{0, 0, 0};
+        AtmServiceFunction.withdraw(chooseWithdrawAmount);
     }
 }
--- src/main/java/AtmServiceFunction.java
+++ src/main/java/AtmServiceFunction.java
@@ -1,62 +1,88 @@
 public class AtmServiceFunction {
     static final int[] bankBills = {1000, 500, 100};
-    static int[] amountOfEachBill = {2, 3, 5};
-    static int[] billCounter = {0, 0, 0};
-    static int balance;
+    static int[] numberOfEachBillInAccount = {2, 3, 5};
+    static int[] numberOfEachBillToPayOut = {0, 0, 0};
+    static int balance = calculateBalance();
 
-    static {
-        calculateBalance();
-    }
+    private static int calculateBalance() {
 
-    private static void calculateBalance() {
+        int balance = 0;
+
         for (int i = 0; i < bankBills.length; i++) {
-            balance = balance + (bankBills[i] * amountOfEachBill[i]);
+            balance = balance + (bankBills[i] * numberOfEachBillInAccount[i]);
         }
+
+        AtmServiceFunction.balance = balance;
+
+        return balance;
     }
 
 
     public static void checkBanknotes() {
+
+
         for (int i = 0; i < bankBills.length; i++) {
-            System.out.println(bankBills[i] + " * " + amountOfEachBill[i] + " = " + bankBills[i] * amountOfEachBill[i]);
+            System.out.println(bankBills[i] + " * " + numberOfEachBillInAccount[i] + " = " + bankBills[i] * numberOfEachBillInAccount[i]);
         }
+
         System.out.println("Total balance = " + balance);
     }
 
     public static void showTotalBalance() {
+
         System.out.println("Total balance: " + balance);
     }
 
 
     public static void withdraw(int chooseWithdrawAmount) {
 
-        if (chooseWithdrawAmount > balance || chooseWithdrawAmount % 100 != 0){
+        if (chooseWithdrawAmount > balance || chooseWithdrawAmount % 100 != 0) {
             System.out.println("This amount is not available! Try again!");
 
         } else {
-            for (int i = 0; i < bankBills.length; i++) {
-                    int countNotes = chooseWithdrawAmount / bankBills[i];
-                        billCounter[i] = Math.min(countNotes, amountOfEachBill[i]);
-                        amountOfEachBill[i] = countNotes >= amountOfEachBill[i] ? 0 : amountOfEachBill[i] - countNotes;
-                        balance = balance - (billCounter[i] * bankBills[i]);
-                        chooseWithdrawAmount = chooseWithdrawAmount - (billCounter[i] * bankBills[i]);
-            }
-            showWithdrawAmount();
-            System.out.println("**********");
-            showLeftBanknotes();
+            payOut(chooseWithdrawAmount);
         }
     }
 
+    private static void payOut(int chooseWithdrawAmount) {
+        boolean pay = true;
+
+        for (int i = 0; i < bankBills.length; i++) {
+            int numberOfBillsToPayOut = chooseWithdrawAmount / bankBills[i];
+
+
+            if (numberOfBillsToPayOut > numberOfEachBillInAccount[i]) {
+                pay = false;
+                System.out.println("Withdrawal declined. \nThere is only " + numberOfEachBillInAccount[i] + " " + bankBills[i] + " left in your account.");
+
+            } else if (numberOfBillsToPayOut == 0) {
+                continue;
+            } else {
+                pay = true;
+                numberOfEachBillToPayOut[i] = numberOfBillsToPayOut;
+                numberOfEachBillInAccount[i] = numberOfEachBillInAccount[i] - numberOfBillsToPayOut;
+            }
+
+            balance = calculateBalance();
+            chooseWithdrawAmount = chooseWithdrawAmount - (numberOfBillsToPayOut * bankBills[i]);
+        }
+        if (pay)
+            showWithdrawAmount();
+        System.out.println("--------------------------------");
+        showLeftBanknotes();
+    }
+
     private static void showWithdrawAmount() {
-        for (int i = 0; i < billCounter.length; i++) {
-            if (billCounter[i] != 0) {
-                System.out.println(bankBills[i] + " * " + billCounter[i] + " = " + (bankBills[i] * billCounter[i]));
+        for (int i = 0; i < numberOfEachBillToPayOut.length; i++) {
+            if (numberOfEachBillToPayOut[i] != 0) {
+                System.out.println(bankBills[i] + " * " + numberOfEachBillToPayOut[i] + " = " + (bankBills[i] * numberOfEachBillToPayOut[i]));
             }
         }
     }
 
     public static void showLeftBanknotes() {
         for (int i = 0; i < bankBills.length; i++) {
-            System.out.println("Notes of " + bankBills[i] + " left are " + amountOfEachBill[i]);
+            System.out.println("Notes of " + bankBills[i] + " left are " + numberOfEachBillInAccount[i]);
 
         }
     }
